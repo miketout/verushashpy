cdfcee06f6295662ea47303e9323d14ee07fbd42
miketout <michaeltoutonghi@gmail.com>
2020-02-17 21:52:05 -0800

    Initialization of VerusHash native module for Python 3.5+

---
+++ README.md
@@ -0,0 +1,76 @@
+VerusHas Module in C++
+======================
+
+An example project built with [pybind11](https://github.com/pybind/pybind11).
+
+Installation (requires Python 3.5+)
+-----------------------------------
+
+**On Unix (Linux, OS X)**
+
+ - clone this repository
+ - `pip install ./verushashpy`
+
+**On Windows (Requires Visual Studio 2015)**
+
+     - clone this repository
+     - `pip install .\verushashpy`
+
+   Note that this requires the user building `verushash` to have registry editing
+   rights on the machine, to be able to run the `vcvarsall.bat` script.
+
+
+Windows runtime requirements
+----------------------------
+
+On Windows, the Visual C++ 2015 redistributable packages are a runtime
+requirement for this project. It can be found [here](https://www.microsoft.com/en-us/download/details.aspx?id=48145).
+
+If you use the Anaconda python distribution, you may require the Visual Studio
+runtime as a platform-dependent runtime requirement for you package:
+
+```yaml
+requirements:
+  build:
+    - python
+    - setuptools
+    - pybind11
+
+  run:
+   - python
+   - vs2015_runtime  # [win]
+```
+
+
+Building the documentation
+--------------------------
+
+Documentation for the example project is generated using Sphinx. Sphinx has the
+ability to automatically inspect the signatures and documentation strings in
+the extension module to generate beautiful documentation in a variety formats.
+The following command generates HTML-based reference documentation; for other
+formats please refer to the Sphinx manual:
+
+ - `cd verushashpy/docs`
+ - `make html`
+
+License
+-------
+
+pybind11 is provided under a BSD-style license that can be found in the LICENSE
+file. By using, distributing, or contributing to this project, you agree to the
+terms and conditions of this license.
+
+Usage
+-----
+
+```python
+import verushash as vh
+
+vh.initialize()
+byte_ary = vh.verushash()
+byte_ary = vh.verushash_V2()
+byte_ary = vh.verushash_V2b()
+byte_ary = vh.verushash_v2b1()
+
+```
---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+// Python callable version of verushash-node for electrumx
+//
+//
+
+#include <stdint.h>
+#include <vector>
+
+#include "crypto/verus_hash.h"
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+CVerusHash* vh;
+CVerusHashV2* vh2;
+CVerusHashV2* vh2b1;
+
+bool initialized = false;
+
+void initialize()
+{
+    if (!initialized)
+    {
+        CVerusHash::init();
+        CVerusHashV2::init();
+    }
+    
+    vh = new CVerusHash();
+    vh2 = new CVerusHashV2(SOLUTION_VERUSHHASH_V2);
+    vh2b1 = new CVerusHashV2(SOLUTION_VERUSHHASH_V2_1);
+    
+    initialized = true;
+}
+
+py::bytes verushash(const std::string bytes) {
+    char *result = new char[32];
+    
+    if (initialized == false) {
+        initialize();
+    }
+    verus_hash(result, bytes.data(), bytes.size());
+    return py::bytes((char *)result, 32);
+}
+
+py::bytes verushash_v2(const std::string bytes) {
+    unsigned char *result = new unsigned char[32];
+    
+    if (initialized == false) {
+        initialize();
+    }
+
+    vh2->Reset();
+    vh2->Write((const unsigned char *)bytes.data(), bytes.size());
+    vh2->Finalize(result);
+    return py::bytes((char *)result, 32);
+}
+
+py::bytes verushash_v2b(const std::string bytes) {
+    unsigned char *result = new unsigned char[32];
+    
+    if (initialized == false) {
+        initialize();
+    }
+
+    vh2->Reset();
+    vh2->Write((const unsigned char *)bytes.data(), bytes.size());
+    vh2->Finalize2b(result);
+    return py::bytes((char *)result, 32);
+}
+
+py::bytes verushash_v2b1(const std::string bytes) {
+    unsigned char *result = new unsigned char[32];
+    
+    if (initialized == false) {
+        initialize();
+    }
+
+    vh2b1->Reset();
+    vh2b1->Write((const unsigned char *)bytes.data(), bytes.size());
+    vh2b1->Finalize2b(result);
+    return py::bytes((char *)result, 32);
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(verushash, m) {
+    m.doc() = R"pbdoc(
+        VerusHash native module for Python
+        ----------------------------------
+
+        .. currentmodule:: verushash
+
+        .. autosummary::
+           :toctree: _generate
+
+           initialize
+           verushash
+           verushash_v2
+           verushash_v2b
+           verushash_v2b1
+    )pbdoc";
+
+    m.def("initialize", &initialize, R"pbdoc(
+        Initialize the verushash module
+    )pbdoc");
+
+    m.def("verushash", &verushash, R"pbdoc(
+        VerusHash V1
+    )pbdoc");
+
+    m.def("verushash_v2", &verushash_v2, R"pbdoc(
+        VerusHash V2, return 32 bytes as bytes result
+    )pbdoc");
+
+    m.def("verushash_v2b", &verushash_v2b, R"pbdoc(
+        VerusHash V2b, return 32 bytes as bytes result
+    )pbdoc");
+
+    m.def("verushash_v2b1", &verushash_v2b1, R"pbdoc(
+        VerusHash V2b1, return 32 bytes as bytes result
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = VERSION_INFO;
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
